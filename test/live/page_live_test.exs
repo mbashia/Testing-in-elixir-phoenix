c4dbddc9a6ba03b4c8fd6ca9aff81344e0466bb7
mbashia <mbashiavictor@gmail.com>
2024-08-01 17:07:35 +0300

    add more live view test

--- test/live/page_live_test.exs
+++ test/live/page_live_test.exs
@@ -20,7 +20,45 @@ defmodule ContactFormWeb.PageLiveTest do
       assert html =~ "Submit"
     end
 
-    test "validates inputs", %{conn: conn} do
+    test "validate first name presence", %{conn: conn} do
+      {:ok, live_view, _html} = live(conn, Routes.page_index_path(conn, :index))
+
+      live_view
+      |> form("#contact-request-form",
+        client: %{
+          first_name: "",
+          last_name: "Doe",
+          email: "JohnDoe@gmail.com",
+          query_type: "General inquiry",
+          message: "this is a message",
+          contact_consent: true
+        }
+      )
+      |> render_change()
+
+      assert render(live_view) =~ "This field is required"
+    end
+
+    test "validate last name presence", %{conn: conn} do
+      {:ok, live_view, _html} = live(conn, Routes.page_index_path(conn, :index))
+
+      live_view
+      |> form("#contact-request-form",
+        client: %{
+          first_name: "John",
+          last_name: "",
+          email: "JohnDoe@gmail.com",
+          query_type: "General inquiry",
+          message: "this is a message",
+          contact_consent: true
+        }
+      )
+      |> render_change()
+
+      assert render(live_view) =~ "This field is required"
+    end
+
+    test "validate email presence", %{conn: conn} do
       {:ok, live_view, _html} = live(conn, Routes.page_index_path(conn, :index))
 
       live_view
@@ -28,7 +66,83 @@ defmodule ContactFormWeb.PageLiveTest do
         client: %{
           first_name: "John",
           last_name: "Doe",
-          email: "invalid",
+          email: "",
+          query_type: "General inquiry",
+          message: "this is a message",
+          contact_consent: true
+        }
+      )
+      |> render_change()
+
+      assert render(live_view) =~ "can&#39;t be blank"
+    end
+
+    test "check valid email", %{conn: conn} do
+      {:ok, live_view, _html} = live(conn, Routes.page_index_path(conn, :index))
+
+      live_view
+      |> form("#contact-request-form",
+        client: %{
+          first_name: "John",
+          last_name: "Doe",
+          email: "invalid_email",
+          query_type: "General inquiry",
+          message: "this is a message",
+          contact_consent: true
+        }
+      )
+      |> render_change()
+
+      assert render(live_view) =~ "please enter a valid email address"
+    end
+
+    test "validate message presence", %{conn: conn} do
+      {:ok, live_view, _html} = live(conn, Routes.page_index_path(conn, :index))
+
+      live_view
+      |> form("#contact-request-form",
+        client: %{
+          first_name: "John",
+          last_name: "Doe",
+          email: "johndoe@gmail.com",
+          query_type: "General inquiry",
+          message: "",
+          contact_consent: true
+        }
+      )
+      |> render_change()
+
+      refute render(live_view) =~ "Please select a query type"
+    end
+
+    test "validate query type presence", %{conn: conn} do
+      {:ok, live_view, _html} = live(conn, Routes.page_index_path(conn, :index))
+
+      live_view
+      |> form("#contact-request-form",
+        client: %{
+          first_name: "John",
+          last_name: "Doe",
+          email: "johndoe@gmail.com",
+          query_type: "General inquiry",
+          message: "this is a message",
+          contact_consent: true
+        }
+      )
+      |> render_change()
+
+      refute render(live_view) =~ "This field is required"
+    end
+
+    test "validate contact consent required", %{conn: conn} do
+      {:ok, live_view, _html} = live(conn, Routes.page_index_path(conn, :index))
+
+      live_view
+      |> form("#contact-request-form",
+        client: %{
+          first_name: "John",
+          last_name: "Doe",
+          email: "johndoe@gmail.com",
           query_type: "General inquiry",
           message: "this is a message",
           contact_consent: false
@@ -36,9 +150,28 @@ defmodule ContactFormWeb.PageLiveTest do
       )
       |> render_change()
 
-      refute render(live_view) =~ "can't be blank"
-      assert render(live_view) =~ "please enter a valid email address"
+      assert render(live_view) =~ "To submit this form please allow to be contacted"
     end
+
+    # test "validates inputs", %{conn: conn} do
+    #   {:ok, live_view, _html} = live(conn, Routes.page_index_path(conn, :index))
+
+    #   live_view
+    #   |> form("#contact-request-form",
+    #     client: %{
+    #       first_name: "John",
+    #       last_name: "Doe",
+    #       email: "invalid",
+    #       query_type: "General inquiry",
+    #       message: "this is a message",
+    #       contact_consent: false
+    #     }
+    #   )
+    #   |> render_change()
+
+    #   refute render(live_view) =~ "can't be blank"
+    #   assert render(live_view) =~ "please enter a valid email address"
+    # end
 
     test "cannot submit form with invalid inputs", %{conn: conn} do
       {:ok, live_view, _html} = live(conn, Routes.page_index_path(conn, :index))
@@ -48,7 +181,7 @@ defmodule ContactFormWeb.PageLiveTest do
         client: %{
           first_name: "",
           last_name: "",
-          email: "example@gmail.com",
+          email: "examplegmail.com",
           query_type: "General inquiry",
           message: "this is a message",
           contact_consent: false
@@ -56,7 +189,9 @@ defmodule ContactFormWeb.PageLiveTest do
       )
       |> render_submit()
 
-      assert render(live_view) =~ "can&#39;t be blank"
+      assert render(live_view) =~ "This field is required"
+      assert render(live_view) =~ "To submit this form please allow to be contacted"
+      assert render(live_view) =~ "please enter a valid email address"
     end
 
     test "submits with valid inputs", %{conn: conn} do
@@ -70,7 +205,7 @@ defmodule ContactFormWeb.PageLiveTest do
           email: "example@gmail.com",
           query_type: "General inquiry",
           message: "this is a message",
-          contact_consent: false
+          contact_consent: true
         }
       )
       |> render_submit()
