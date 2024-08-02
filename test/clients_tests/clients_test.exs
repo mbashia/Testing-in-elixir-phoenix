5e25b91a326b47800704adcf5b17ff7d60581d46
mbashia <mbashiavictor@gmail.com>
2024-08-02 07:21:22 +0300

    add more test

--- test/clients_tests/clients_test.exs
+++ test/clients_tests/clients_test.exs
@@ -5,17 +5,26 @@ defmodule ContactFormWeb.ClientsTest do
   alias ContactForm.ClientsRequests.Client
   alias ContactForm.ClientsRequests.Clients
 
-  test "create_contact_request_record/1" do
-    attrs = %{
-      first_name: "John",
-      last_name: "Doe",
-      email: "johndoe@gmail.com",
-      query_type: "General inquiry",
-      message: "this is a message",
-      contact_consent: false
-    }
+  @valid_attrs %{
+    first_name: "John",
+    last_name: "Doe",
+    email: "johndoe@gmail.com",
+    query_type: "General inquiry",
+    message: "this is a message",
+    contact_consent: false
+  }
 
-    assert {:ok, record} = Clients.create_contact_request_record(attrs)
+  @invalid_attrs %{
+    first_name: "John",
+    last_name: "Doe",
+    email: "invalidemail",
+    query_type: "",
+    message: "this is a message",
+    contact_consent: false
+  }
+
+  test "create_contact_request_record/1 with valid data" do
+    assert {:ok, record} = Clients.create_contact_request_record(@valid_attrs)
     assert record.first_name == "John"
     assert record.last_name == "Doe"
     assert record.email == "johndoe@gmail.com"
@@ -24,11 +33,27 @@ defmodule ContactFormWeb.ClientsTest do
     assert record.contact_consent == false
   end
 
-  test "change_contact_form/2" do
-    client = %Client{}
-    attrs = %{first_name: "John", last_name: "Doe", email: "john@example.com"}
+  test "create_contact_request_record/1 with invalid data returns invalid changeset" do
+    assert {:error, %Ecto.Changeset{}} = Clients.create_contact_request_record(@invalid_attrs)
+  end
 
-    assert changeset = Clients.change_contact_form(client, attrs)
+  test "change_contact_form/2 returns contact request changeset" do
+    client = %Client{}
+
+    assert %Ecto.Changeset{} = Clients.change_contact_form(client, @valid_attrs)
+  end
+
+  test "change_contact_form/2 returns valid changeset with valid data " do
+    client = %Client{}
+
+    assert changeset = Clients.change_contact_form(client, @valid_attrs)
+    assert changeset.valid? == true
+  end
+
+  test "change_contact_form/2 returns returns a false changeset with invalid data" do
+    client = %Client{}
+
+    assert changeset = Clients.change_contact_form(client, @invalid_attrs)
     assert changeset.valid? == false
   end
 end
