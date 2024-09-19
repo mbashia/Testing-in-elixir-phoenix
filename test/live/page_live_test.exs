b640873b09d1abfe800c9f054a4319aa2bc57974
mbashia <mbashiavictor@gmail.com>
2024-09-19 09:06:48 +0300

    comment code

--- test/live/page_live_test.exs
+++ test/live/page_live_test.exs
@@ -6,7 +6,10 @@ defmodule ContactFormWeb.PageLiveTest do
 
   describe("Contact Form ") do
     test "form renders correctly", %{conn: conn} do
+      # Opens a LiveView session and fetches the HTML of the rendered form
+
       {:ok, _live_view, html} = live(conn, Routes.page_index_path(conn, :index))
+      # Checks that the HTML contains the expected text for form fields and buttons
 
       assert html =~ "First Name"
       assert html =~ "Last Name"
@@ -22,6 +25,7 @@ defmodule ContactFormWeb.PageLiveTest do
 
     test "cannot submit form with invalid inputs", %{conn: conn} do
       {:ok, live_view, _html} = live(conn, Routes.page_index_path(conn, :index))
+      # Simulate filling out the form with invalid data
 
       live_view
       |> form("#contact-request-form",
@@ -34,7 +38,10 @@ defmodule ContactFormWeb.PageLiveTest do
           contact_consent: false
         }
       )
+      # Submit the form with invalid data
       |> render_submit()
+
+      # Check that the form displays appropriate error messages
 
       assert render(live_view) =~ "This field is required"
       assert render(live_view) =~ "To submit this form, please consent to being contacted"
@@ -43,6 +50,7 @@ defmodule ContactFormWeb.PageLiveTest do
 
     test "submits with valid inputs", %{conn: conn} do
       {:ok, live_view, _html} = live(conn, Routes.page_index_path(conn, :index))
+      # Simulate filling out the form with valid data
 
       live_view
       |> form("#contact-request-form",
@@ -55,7 +63,10 @@ defmodule ContactFormWeb.PageLiveTest do
           contact_consent: true
         }
       )
+      # Submit the form with valid data
       |> render_submit()
+
+      # Check that the form displays a success message
 
       assert render(live_view) =~ "Thanks for completing the form,we&#39;ll be in touch soon!"
     end
